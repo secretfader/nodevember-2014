97b224c13df77e529155058c1708337f789b9970
Nicholas Young <nicholas@nicholaswyoung.com>
2014-11-16 13:33:35 -0600

    Revert "Fix publisher IP."
    This reverts commit 576359115a78fd83320e5662fdbdbe54d847b583.

--- publisher/app.js
+++ publisher/app.js
@@ -1,18 +1,24 @@
-var zmq    = require('zmq')
-,   socket = zmq.socket('push')
-,   ip     = '10.200.221.29';
+var zmq    = require('zmq');
 
-socket.identity = ['upstream', 'quotes', process.pid].join('-');
-
-socket.bind('tcp://' + ip + ':12345', function (err) {
+// Wrapper to setup current IP address.
+require('dns').lookup(require('os').hostname(), function (err, ip) {
   if (err) throw err;
 
-  var stocks = ['APPL', 'GOOG', 'FB', 'YHOO'];
+  var socket = zmq.socket('push');
 
-  setInterval(function () {
-    var symbol = stocks[Math.floor(Math.random() * stocks.length)]
-    ,   quote  = (Math.random() * 100.0 + 25.0).toFixed(2);
+  socket.identity = ['upstream', 'quotes', process.pid].join('-');
 
-    socket.send(JSON.stringify({ symbol: symbol, quote: quote }));
-  }, 500);
+  socket.bind('tcp://' + ip + ':12345', function (err) {
+    if (err) throw err;
+
+    var stocks = ['APPL', 'GOOG', 'FB', 'YHOO'];
+
+    setInterval(function () {
+      var symbol = stocks[Math.floor(Math.random() * stocks.length)]
+      ,   quote  = (Math.random() * 100.0 + 25.0).toFixed(2);
+
+      socket.send(JSON.stringify({ symbol: symbol, quote: quote }));
+    }, 500);
+  });
 });
+// End wrapper.
