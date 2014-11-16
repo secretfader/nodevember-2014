dcee38e21952d44a9392fe9b7c73df39ac008f4f
Nicholas Young <nicholas@nicholaswyoung.com>
2014-11-16 13:33:47 -0600

    Revert "Make it interactive!"
    This reverts commit 924d3f399211ef8fb9f3a00de8a590cba023c4fa.

--- publisher/app.js
+++ publisher/app.js
@@ -1,24 +1,17 @@
-var zmq    = require('zmq');
+var zmq    = require('zmq')
+,   socket = zmq.socket('push');
 
-// Wrapper to setup current IP address.
-require('dns').lookup(require('os').hostname(), function (err, ip) {
+socket.identity = ['upstream', 'quotes', process.pid].join('-');
+
+socket.bind('tcp://127.0.0.1:12345', function (err) {
   if (err) throw err;
 
-  var socket = zmq.socket('push');
+  var stocks = ['APPL', 'GOOG', 'FB', 'YHOO'];
 
-  socket.identity = ['upstream', 'quotes', process.pid].join('-');
+  setInterval(function () {
+    var symbol = stocks[Math.floor(Math.random() * stocks.length)]
+    ,   quote  = (Math.random() * 100.0 + 25.0).toFixed(2);
 
-  socket.bind('tcp://' + ip + ':12345', function (err) {
-    if (err) throw err;
-
-    var stocks = ['APPL', 'GOOG', 'FB', 'YHOO'];
-
-    setInterval(function () {
-      var symbol = stocks[Math.floor(Math.random() * stocks.length)]
-      ,   quote  = (Math.random() * 100.0 + 25.0).toFixed(2);
-
-      socket.send(JSON.stringify({ symbol: symbol, quote: quote }));
-    }, 500);
-  });
+    socket.send(JSON.stringify({ symbol: symbol, quote: quote }));
+  }, 500);
 });
-// End wrapper.
--- web/app.js
+++ web/app.js
@@ -5,68 +5,54 @@ var path    = require('path')
 ,   db      = require('../db')
 ,   emitter = require('./lib/stocks_emitter')
 ,   socket  = zmq.socket('pull')
-,   app     = module.exports = express()
+,   app     = express()
 
-// Wrapper to setup current IP address.
-require('dns').lookup(require('os').hostname(), function (err, ip) {
-  if (err) throw err;
+/**
+ * Connect to MongoDB
+ */
+db.connect('mongodb://localhost/nodevember_2014');
 
-  /**
-   * Connect to MongoDB
-   */
-  db.connect('mongodb://localhost/nodevember_2014');
+/**
+ * Log incoming data from the publisher.
+ */
+var Stock = db.model('Stock')
+,   Quote = db.model('Quote');
 
-  /**
-   * Log incoming data from the publisher.
-   */
-  var Stock = db.model('Stock')
-  ,   Quote = db.model('Quote');
-
-  emitter.on('data', function (data) {
-    data = JSON.parse(data);
-    // .....
-  });
-
-  /**
-   * Setup 0MQ
-   */
-  socket.identity = ['downstream', 'quotes', process.pid].join('-');
-  socket.connect('tcp://' + ip + ':12345');
-  socket.on('message', function (data) {
-    emitter.write(data);
-  });
-
-  /**
-   * App Settings
-   */
-  app.set('views', require('path').join(__dirname, 'views'));
-  app.set('view engine', 'jade');
-
-  /**
-   * Locals
-   */
-  app.locals.ip = ip;
-
-  /**
-   * Configure HTTP Middleware
-   */
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  /**
-   * HTTP Routes
-   */
-  app.route('/').get(function (req, res) {
-    res.render('dashboard');
-  });
-
-  app.route('/ticker')
-    .all(sse())
-    .get(function (req, res) {
-      emitter.on('data', function (data) {
-        res.json(JSON.parse(data));
-      })
-    });
-
-  app.listen(8000);
+emitter.on('data', function (data) {
+  data = JSON.parse(data);
+  // .....
 });
-// End wrapper.
+
+/**
+ * Setup 0MQ
+ */
+socket.identity = ['downstream', 'quotes', process.pid].join('-');
+socket.connect('tcp://127.0.0.1:12345');
+socket.on('message', function (data) {
+  emitter.write(data);
+});
+
+/**
+ * Configure HTTP Middleware
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * HTTP Routes
+ */
+app.route('/').get(function (req, res) {
+  res.send('Hello, Nodevember!');
+});
+
+app.route('/ticker')
+  .all(sse())
+  .get(function (req, res) {
+    emitter.on('data', function (data) {
+      res.json(JSON.parse(data));
+    })
+  });
+
+/**
+ * Boot the HTTP server
+ */
+app.listen(8000);
