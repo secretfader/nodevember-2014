576359115a78fd83320e5662fdbdbe54d847b583
Nicholas Young <nicholas@nicholaswyoung.com>
2014-11-16 10:48:44 -0600

    Fix publisher IP.

--- publisher/app.js
+++ publisher/app.js
@@ -1,24 +1,18 @@
-var zmq    = require('zmq');
+var zmq    = require('zmq')
+,   socket = zmq.socket('push')
+,   ip     = '10.200.221.29';
 
-// Wrapper to setup current IP address.
-require('dns').lookup(require('os').hostname(), function (err, ip) {
+socket.identity = ['upstream', 'quotes', process.pid].join('-');
+
+socket.bind('tcp://' + ip + ':12345', function (err) {
   if (err) throw err;
 
-  var socket = zmq.socket('push');
+  var stocks = ['APPL', 'GOOG', 'FB', 'YHOO'];
 
-  socket.identity = ['upstream', 'quotes', process.pid].join('-');
+  setInterval(function () {
+    var symbol = stocks[Math.floor(Math.random() * stocks.length)]
+    ,   quote  = (Math.random() * 100.0 + 25.0).toFixed(2);
 
-  socket.bind('tcp://' + ip + ':12345', function (err) {
-    if (err) throw err;
-
-    var stocks = ['APPL', 'GOOG', 'FB', 'YHOO'];
-
-    setInterval(function () {
-      var symbol = stocks[Math.floor(Math.random() * stocks.length)]
-      ,   quote  = (Math.random() * 100.0 + 25.0).toFixed(2);
-
-      socket.send(JSON.stringify({ symbol: symbol, quote: quote }));
-    }, 500);
-  });
+    socket.send(JSON.stringify({ symbol: symbol, quote: quote }));
+  }, 500);
 });
-// End wrapper.
